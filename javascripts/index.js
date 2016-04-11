5d02790ff367bd80669e53a307b3f934347677b5
Jordan <powellj@mit.edu>
2016-04-11 19:06:00 -0400

    more modal stuff I guess

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,20 +5,26 @@ $(function() {
 	john = {id: "One",
 			name: "John",
 			age: 13,
-			budget: 50}
+			budget: 50,
+			toyChest: ["Dinosaur", "Gameboy", "Lego", "Racecar", "Cards", "other toys"]}
 
 	david = {id: "Two",
 			name: "David",
 			age:8,
-			budget: 50}
+			budget: 50,
+			toyChest: ["Connect 4", "Chess", "Book", "Xbox", "Wii", "yet another toy"]}
 	annie = {id: "three",
 			name: "Annie",
 			age: 22,
-			budget: 1000}
+			budget: 1000,
+			toyChest: ["camera", "metal thing", "laptop", "tennis ball"]}
 
 	var children = [john, david, annie];
 	var firstItem = true;
 	var offset;
+	var test = function(){
+		console.log("gets called");
+	}
 	children.forEach(function(child){
 		console.log(firstItem);
 		offset = "";
@@ -48,10 +54,23 @@ $(function() {
 		         '</p>'+
 		      '</div>'+
 		   	'</div>'
-			)
+			);
 		$("#"+child.id).find(".caption").find(".home-profile-text").find(".name").text(child.name);
 		$("#"+child.id).find(".caption").find(".home-profile-text").find(".age").text(child.age);
 		$("#"+child.id).find(".caption").find(".home-profile-text").find(".budget").text(child.budget);
+
+
+		$("#toyChestModal").append( 
+			'<div class = "thumbnail col-md-6">'+
+			'<h3 class="text-center">'+child.name+'</h3>'+
+			'<ul id="'+child.id+'toyChest" class = "list-group checked-list-box">'+
+			'</ul></div>'
+			);
+
+		child.toyChest.forEach(function(toyName){
+			$("#"+child.id+"toyChest").append(
+				'<a href="#" class="list-group-item">'+toyName+'<img src="../images/testToyPicture.png"></a>'
+			);
+		});
 	});
-	
 });
