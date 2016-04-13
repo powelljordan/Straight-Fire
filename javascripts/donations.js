f295732cddc3f1819ad8fd6e6b1317ba17f57bd2
cwillaim <cwwomack@mit.edu>
2016-04-12 20:02:30 -0400

    implemented return to toyChest and link to add donations modal

--- javascripts/donations.js
+++ javascripts/donations.js
@@ -1,24 +1,42 @@
 $(function() {
 	var lastDeleted;
-	$('.donate-btn').tooltip({
-		trigger: 'hover focus',
-		title: 'Mark as donated',
-		placement: 'top'
+
+	var baseToys = $('.items').children('.item-col');
+	baseToys.each(function(index, item) {
+		$('.items').append($(item).clone());
+	});
+	baseToys.each(function(index, item) {
+		$('.items').append($(item).clone());
 	});
 
-	$('.donate-btn').click(function() {
+	var deleteAction = function() {
+		var undoMessage = "";
+		if ($(this).hasClass('donate-btn')) {
+			undoMessage = "Mark as donated";
+		} else {
+			undoMessage = "Return to toyChest";
+		}
 		$(this).parents('.item-col').fadeOut({
 			complete: function() {
 				lastDeleted = $(this).addClass('item-hidden');
+				$('#undo-action').text(undoMessage);
 				$('#undo').fadeIn();
 			}
 		});
-	});
+	}
+
+	$('.donate-btn, .return-to-chest').click(deleteAction);
 
 	$('#undo').click(function() {
 		if (lastDeleted) {
 			lastDeleted.removeClass('item-hidden').fadeIn();
 			$(this).fadeOut();
 		}
+	});
+
+	$('.donate-btn').tooltip({
+		trigger: 'hover focus',
+		title: 'Mark as donated',
+		placement: 'top'
 	});
 });
