4b25a3c4647bcec9dcc68c4e806c895b35a4c0b3
Jordan <powellj@mit.edu>
2016-04-24 21:57:17 -0400

    changed to materialize look. Navigating to profile from hoem page isn't really possible right now. I messed up something

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,18 +1,21 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 $(function() {
+	$(".button-collapse").sideNav();
 	//Load Children Data
 	john = {id: "One",
 			name: "John",
 			age: 13,
 			budget: 50,
-			toyChest: ["Dinosaur", "Gameboy", "Lego", "Racecar", "Cards", "another", "and another"]}
+			toyChest: ["Dinosaur", "Gameboy", "Lego", "Racecar", "Cards", "another", "and another"],
+			interests:["Biking", "Arts & Crafts", "Star Wars"]}
 
 	david = {id: "Two",
 			name: "David",
 			age:8,
 			budget: 50,
-			toyChest: ["Connect 4", "Chess", "Book", "Xbox", "Wii"]}
+			toyChest: ["Connect 4", "Chess", "Book", "Xbox", "Wii"],
+			interests:["Cars", "Action Figures", "Painting"]}
 
 	var children = [john, david];
 	var firstItem = true;
@@ -61,33 +64,42 @@ $(function() {
 			firstItem = true;
 		}
 		$("#profiles").append(
-			'<div class = "profile col-sm-6 col-md-4 '+offset+'" id="'+child.id+'">'+
-				'<div class="panel child-panel-card">'+
-			      '<div class = "thumbnail">'+
-			         '<img src = "../images/'+ child.name.charAt(0).toLowerCase() + child.name.slice(1) + '.png" alt = "Generic placeholder thumbnail">'+
+			'<div class = "col-xs-6 col-sm-4 col-md-4 " id="'+child.id+'">'+
+				'<div class="card small">'+
+			      '<div class = "card-image waves-effect waves-block waves-light">'+
+			         '<img class="activator" src = "../images/'+ child.name.charAt(0).toLowerCase() + child.name.slice(1) + '.png" alt = "Generic placeholder thumbnail">'+
 			      '</div>'+
-			      
-			      '<div class = "caption">'+
-			         '<p class="home-profile-text">Name: <span class="name"></span></p>'+
-			         '<p class="home-profile-text">Age: <span class="age "></span></p>'+
-			         '<p class="home-profile-text">Budget: <span class="budget"></span></p>'+
-			       '</div><div class="btn-wrapper">'+
+			      '<div class = "card-content" style="text-align:center">'+
+			         '<span class="card-title activator name"></span>'+
+			         '<div class="btn-group" style="text-align:center">'+
 			            '<a href = "#" id="'+ child.name +'" class = "btn btn-default btn-toychest" role = "button">'+
-			               'ToyChest'+
+			               'Toys'+
 			            '</a> '+
-			            '<a href = "#" id="'+ child.name +'"class = "btn btn-default btn-wishlist" role = "button">'+
-			               'Wishlist'+
-			            '</a>'+
 			            '<a href="shopping.html?name='+child.name+'" id="btn-start-shopping" class="btn btn-default" role = "button">' + 
-			            	'<span class = "glyphicon glyphicon-shopping-cart"></span> Start Shopping'+
-			            '</a>' +	            
+			            	' Shop'+
+			            '</a>' +
+			          '</div>'+  
+			       '</div>'+
+			       '<div class="card-reveal style="text-align:center>'+
+			       	 '<span class="card-title grey-text text-darken-4 name"><i class="material-icons right">close</i></span>'+ 
+			       	 // '<span class="age" style="font-size:1.5em">Age: </span>'+
+			       	 '<ul id="interest-list" class="collection with-header">'+
+			       	 '<li class="collection-header"><h6>Interests</h6></li>'+
+			       	 '</ul>'+
+			       	 '<a class="btn bottom center profile">Edit<i class="material-icons">mode edit</i></a>'+         
 			      '</div>'+
 			     '</div>'+
 		   	'</div>'
 			);
-		$("#"+child.id).find(".caption").find(".home-profile-text").find(".name").text(child.name);
-		$("#"+child.id).find(".caption").find(".home-profile-text").find(".age").text(child.age);
-		$("#"+child.id).find(".caption").find(".home-profile-text").find(".budget").text(child.budget);
+		$("#"+child.id).find(".card-content").find(".name").text(child.name);
+		$("#"+child.id).find(".card-reveal").find(".name").html(child.name + '<i class="material-icons right">close</i>');
+		// $("#"+child.id).find(".card-reveal").find(".age").text(child.age);
+		child.interests.forEach(function(interest){
+			$("#"+child.id).find(".card-reveal").find("#interest-list").append(
+	            '<li class="collection-item">'+interest+'</li>'
+            );
+		});
+
 
 
 		$("#toyChestModal").append( 
@@ -109,6 +121,17 @@ $(function() {
 			);
 		});
 	});
+
+	$("#profiles").append(
+		'<div id="btn-new-profile" class = "col-xs-6 col-sm-4 col-md-4" id="btn-create-profile">'+
+			'<div class="card small">'+
+		      '<div class = "card-image waves-effect waves-block waves-light" style="padding-top:5em">'+
+		      '<i class="material-icons large" style=" opacity:0.6; display:block; text-align:center; vertical-align:middle">add</i>'+
+		      '</div>'+
+		      '<div class="card-content" style="text-align:center; font-size:2em; padding-top:2em">Add another child</div>'+
+		    '</div>'+
+	   	'</div>'
+		);
 
 	$(".profile").click(function(event){
 		// console.log(event.target);
@@ -149,5 +172,16 @@ $(function() {
 		var name = event.toElement.id;
 		$("#toychest-modal").modal("show");
 		$("#toychest-name").text(name);
+	});
+
+	$("#manageProfiles").click(function(event){
+		children.forEach(function(child){
+			$("#"+child.id)
+			.find('.card')
+			.find('.card-reveal')
+			.css({ display: 'block'})
+			.velocity("stop", false)
+			.velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
+		});
 	});
 });
